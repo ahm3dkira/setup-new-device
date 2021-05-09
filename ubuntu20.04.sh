412a0224ce7fb91db5200e5cce674ed3c6b2cd28
Ahmed Kira <50806393+MrKira128@users.noreply.github.com>
2021-05-09 20:14:04 +0200

    Update ubuntu20.04.sh

--- ubuntu20.04.sh
+++ ubuntu20.04.sh
@@ -22,6 +22,20 @@ kdenlive
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
+
+
+
+
+
+
+sudo apt purge --ignore-missing -y \
+kdeconnectd 
+
+
+
+
+
+
 ## code exodus xmrig discord
 ##
 #info
